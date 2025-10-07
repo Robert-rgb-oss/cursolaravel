1c9062ee5aa39bf23934d0f8fa24b9f668ab4590
Robert <Robertseguridadinformatica@gmail.com>
2025-10-07 18:40:43 +0100

    act

--- crud/app/Http/Controllers/NoteController.php
+++ crud/app/Http/Controllers/NoteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Note;
 use App\Http\Request\NoteRequest;
 
