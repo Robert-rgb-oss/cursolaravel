90ffcaa1e01947c74a6e209cedacd904ca0408d0
Robert <Robertseguridadinformatica@gmail.com>
2025-10-07 20:11:42 +0100

    act

---
+++ crud/resources/views/layouts/_partials/messages.blade.php
@@ -0,0 +1,10 @@
+@if(Session::get('success'))
+    <div style="padding:15px; background-color:green; color:white;">
+        <p>{{ $message }}</p>
+    </div>
+@endif
+@if(Session::get('danger'))
+    <div style="padding:15px; background-color:red; color:white;">
+        <p>{{ $message }}</p>
+</div>
+@endif
