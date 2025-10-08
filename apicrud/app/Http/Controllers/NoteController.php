69ed48587fc4612ff51357c161df869331b3153e
Robert <Robertseguridadinformatica@gmail.com>
2025-10-08 19:55:50 +0100

    act

--- apicrud/app/Http/Controllers/NoteController.php
+++ apicrud/app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Requests\NoteRequest;
 use App\Models\Note;
 
 class NoteController extends Controller
--- apicrud/routes/api.php
+++ apicrud/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NoteControler;
+use App\Http\Controllers\NoteController;
 
 Route::resource('/note', NoteController::class);
 
