4fa20b0cc84016fcbfc9d0bb364e4d5742d3f2ac
Robert <Robertseguridadinformatica@gmail.com>
2025-10-08 19:19:41 +0100

    act

--- apicrud/app/Http/Controllers/NoteController.php
+++ apicrud/app/Http/Controllers/NoteController.php
@@ -6,18 +6,13 @@ use App\Models\Note;
 
 class NoteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
         $notes = Note::all();
         return response()->json($notes,200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(NoteRequest $request)
     {
         Note::create($request->all());
@@ -26,9 +21,7 @@ class NoteController extends Controller
         ],201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(NoteRequest $request)
     {
         Note::create($request->all());
@@ -37,37 +30,30 @@ class NoteController extends Controller
         ],201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show($id)
     {
         $note = Note::find($id);
         return response()->json($note, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+  
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(NoteRequest $request, $id)
     {
         $note = Note::find($id);
         $note->title = $request->title;
         $note->description = $request->description;
+        $note->save();
         
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
         //
