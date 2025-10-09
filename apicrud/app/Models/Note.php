14060ac4404400fe763e678488567778a2f78f36
Robert <Robertseguridadinformatica@gmail.com>
2025-10-09 16:23:52 +0100

    act

--- apicrud/app/Models/Note.php
+++ apicrud/app/Models/Note.php
@@ -5,8 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
-{
-      protected $fillable = [
+{;
+      protected $guarded = []
+
+      protected $guarded = []
+
+      protected $hidden = []
+      
         'title',
         'description',
         
