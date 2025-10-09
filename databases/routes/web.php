2ce0a29ca51a6c499bd3bfa15c9bd2050426a293
Robert <Robertseguridadinformatica@gmail.com>
2025-10-09 19:01:34 +0100

    act

--- databases/routes/web.php
+++ databases/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/')->name('product.index');
+Route::get('/', [ProductController::class, 'index']);
