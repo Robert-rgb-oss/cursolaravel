c95ea701ea5a9126400140395219b8b57927a67a
Robert <Robertseguridadinformatica@gmail.com>
2025-10-09 17:03:29 +0100

    act

--- apicrud/app/Http/Resources/NoteResource.php
+++ apicrud/app/Http/Resources/NoteResource.php
@@ -7,26 +7,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class NoteResource extends JsonResource
 {
-
-    public function toArray(Request $request):  
-
-    {
-        return[
-            'id'=> $this->id,
-            'title'=> $'Title' . $this->title,
-            'description'=> $this->description,
-            'example' => 'This is an example.'
-        ];
-    }
-}
-
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+    
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'=> $this->id,
+            'title'=> 'Title' . $this->title,
+            'description'=> $this->description,
+            'example'=> 'This is an example.'
+        ];
     }
 }
