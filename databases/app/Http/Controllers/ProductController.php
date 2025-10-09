5a98908689843aed5e172650b74629d5ecad9071
Robert <Robertseguridadinformatica@gmail.com>
2025-10-09 17:57:52 +0100

    act

---
+++ databases/app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index():View
+    {
+        $products = Product::all();
+        return view('product.index', compact('products'));
+    }
+}
