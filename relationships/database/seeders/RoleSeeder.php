97365d058f90251aa590f26ed28880a094d57954
Robert <Robertseguridadinformatica@gmail.com>
2025-10-14 19:14:50 +0100

    act

--- relationships/database/seeders/RoleSeeder.php
+++ relationships/database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use ºApp\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       Role::create([
+            'id'=> 1,
+            'name' => 'admin'
+       ]);
+         Role::create([
+                'id'=> 2,
+                'name' => 'staff'
+         ]);
+         Role::create([
+                'id'=> 3,
+                'name' => 'user'
+         ]);
+         Role::create([
+                'id'=> 4,
+                'name' => 'guest'
+         ]);
     }
 }
