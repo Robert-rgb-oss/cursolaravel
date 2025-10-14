868b9739acf25969a598816521a3c83d79df76b8
Robert <Robertseguridadinformatica@gmail.com>
2025-10-14 17:55:44 +0100

    act

--- relationships/resources/views/index.blade.php
+++ relationships/resources/views/index.blade.php
@@ -8,8 +8,10 @@
 <body>
     <h1>{{ $user->name }} Phones:</h1>
     <ul>
-        @foreach ($user->phone as $phone)
+        @foreach($user->phones as $phone)
         <li>{{ $phone->prefix }} {{ $phone->phone_number }}</li>
         @endforeach
+    </ul>
+
 </body>
 </html>
